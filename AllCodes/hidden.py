b7c72543107a8732f8939fffabcc6e3083eeb0f5
Dipendu Chanda <chandadipendu@gmail.com>
2017-07-29 16:39:53 -0500

    Add files via upload

---
+++ AllCodes/hidden.py
@@ -0,0 +1,8 @@
+# Keep this file separate
+# https://apps.twitter.com/
+
+def oauth() :
+    return { "consumer_key" : "FJYD8HelrOJZ4TAnKqXwMzyXo",
+        "consumer_secret" : "820K08VvdP8vBHqWyEnqc2gMVJcU5PstVgmPGzmsvteS5iYU6l",
+        "token_key" : "532811068-4tpOZbsLyWnEdN09lHkTfurKusBhmY9ZufQSkvcw",
+        "token_secret" : "FxW1d9tMV47DvFYRkzd7Ipgac2Ex44ptbPdxZAxTO6vSF" }
